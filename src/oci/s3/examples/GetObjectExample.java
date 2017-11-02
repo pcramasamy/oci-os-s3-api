a5cd93d7197e8aca4ce20e069365cc0b31fc6783
pcramasamy <pcramasamy@users.noreply.github.com>
2017-11-01 22:16:14 -0400

    GetObject example initial version including eclipse project file

---
+++ src/oci/s3/examples/GetObjectExample.java
@@ -0,0 +1,41 @@
+package oci.s3.examples;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+import oci.s3.util.ClientUtils;
+
+public class GetObjectExample {
+	
+	public static void main(String args[]) {
+		
+		if (args.length < 3) {
+			System.out.println("java oci.s3.examples.GetObjectExample <bucket> <object> <target_file>");
+			System.exit(1);
+		}
+		
+		AmazonS3 client = null;
+		try {
+			client = ClientUtils.buildAmazonS3Client();
+		} catch (IOException e) {
+			System.out.println("ERROR: OCI Config file not found");
+		}
+		
+		GetObjectRequest getObjectRequest = new GetObjectRequest(args[0], args[1]);
+		S3Object object = client.getObject(getObjectRequest);
+		File targetFile = new File(args[2]);
+		if (targetFile.exists()) {
+			targetFile.delete();
+		}
+
+		try {
+			Files.copy(object.getObjectContent(), targetFile.toPath());
+		} catch (IOException e) {
+			System.out.println("ERROR: "+e.getMessage());
+		}
+	}
+}
---
+++ src/oci/s3/util/ClientUtils.java
@@ -0,0 +1,41 @@
+package oci.s3.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+public class ClientUtils {
+	
+	public static AmazonS3 buildAmazonS3Client() throws IOException {
+		// load oci config file
+		Properties ociConfigProps = new Properties();
+		FileInputStream configStream = new FileInputStream("oci.config");
+		ociConfigProps.load(configStream);
+		configStream.close();
+
+		String region = ociConfigProps.getProperty("oci.region");
+		
+		AWSCredentialsProvider credentials = new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+				ociConfigProps.getProperty("oci.s3_compat.access_key.id"),
+				ociConfigProps.getProperty("oci.s3_compat.access_key")));
+		
+		String endpoint = String.format("%s.compat.objectstorage.%s.oraclecloud.com", ociConfigProps.getProperty("oci.tenancy"), region);
+		AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(endpoint, region);
+		AmazonS3 client = AmazonS3Client.builder().standard()
+		 .withCredentials(credentials)
+		 .withEndpointConfiguration(endpointConfiguration)
+		 .disableChunkedEncoding()
+		 .enablePathStyleAccess()
+		 .build();	
+		
+		return client;
+	}
+
+}
